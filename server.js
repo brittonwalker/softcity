c21cee54c51b2da022fb81aaf1505cd5a02f8aad
Britton Walker <bwalker1801@gmail.com>
2016-03-27 12:44:17 -0400

    add mongolab uri

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/api/private', authCheck, function(req, res) {
 
 // configuration =================
 
-mongoose.connect(db.url); // connect to mongoDB database on modulus.io
+mongoose.connect(env.MONGOLAB_URI || db.url); // connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
