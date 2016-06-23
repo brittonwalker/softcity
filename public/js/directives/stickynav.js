2e4b5037bbc649eb744e2682f633264c65f912c5
Britton Walker <britton@forofficeuseonly.com>
2016-06-22 21:46:21 -0400

    fix nav for mobile

--- public/js/directives/stickynav.js
+++ public/js/directives/stickynav.js
@@ -8,11 +8,20 @@
             return {
                 restrict: 'AC',
                 link: $(window).scroll(function() {
-                    $('nav').addClass('fixie');
-                    $('main').css({'padding-top' : '100px'});
-                    if($(this).scrollTop() === 0){
+                    var isMobile = false;
+
+                    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                      isMobile = true;
+                    }
+
+                    if($(this).scrollTop() >= 50 && isMobile === false){
+                      $('nav').addClass('fixie');
+                      $('main').css({'padding-top' : '100px'});
+                    }
+
+                    if($(this).scrollTop() === 0 && isMobile === false){
                       $('nav').removeClass('fixie');
-                      $('main').css({'padding-top' : '30px'});
+                      $('main').css({'padding-top' : '50px'});
                     }
                 })
             };
